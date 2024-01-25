1a396e3648e42cc75fdcff0b72db3e7dd25aaf75
klearliu <clearlynetworking@gmail.com>
2024-01-25 14:34:51 -0700

    added new files for assignment 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/CardList/cardlist.component';
 
 function App() {
+  
+  const monsters = [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+       "city": "Gwenborough",
+       "zipcode": "92998-3874",
+       "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  }
+];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monster Rolodex</h1>
+      <CardList monsters={monsters}/>
     </div>
   );
 }
---
+++ src/components/CardList/cardlist.component.jsx
@@ -0,0 +1,14 @@
+// cardlist.component.jsx
+import React from "react";
+import { Card } from '../Card/card.component'
+import "./cardlist.styles.css";
+
+export const CardList = ({ monsters }) => (
+    <div className="cardlist">
+        {monsters.map(monster => (
+            <Card key={monster.id} monster={monster} />
+        ))}
+    </div>
+);
+
+export default CardList;
